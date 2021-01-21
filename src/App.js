4f193572d514854b1f177d63444baca238efef1b
nazishedureka <nazish.fraz@edureka.co>
2021-01-22 00:58:23 +0530

    starting code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Button
+} from 'react-bootstrap'
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "paneer butter masala",
+      price: 200
+    },
+    {
+      id: 2,
+      name: "paneer chilli",
+      price: 250
+    },
+    {
+      id: 3,
+      name: "roasted chicken",
+      price: 300
+    }
+  ];
+  let cart_items = [];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">React POS System</Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col sm={8}>
+            <Row>
+              {products.map((product, index) => (
+                <Col sm={2}>
+                  <Button style={{ height: "120px" }}>
+                    {product.name}
+                    <br></br>
+                    (Rs {product.price})
+                  </Button>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col sm={4}>
+            {
+              cart_items.length
+                ? (
+                  <p>sdsdsdsd</p>
+                )
+                : (
+                <p>No item</p>
+              )
+            }
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
